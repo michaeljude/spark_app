a194322e243ff387275fe74211f33626ad42e14c
michaeljude <michaeljude.jmdr@gmail.com>
2021-01-14 08:34:35 +0800

    Fixed app dart

--- lib/core/app.dart
+++ lib/core/app.dart
@@ -4,17 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:logger/logger.dart';
 import 'package:provider/provider.dart';
-import 'package:spark_app/application/dashboard/bottom_navigation/bottom_navigation_bloc.dart';
-import 'package:spark_app/application/dashboard/bottom_navigation/bottom_navigation_event.dart';
-import 'package:spark_app/application/dashboard/bottom_navigation/bottom_navigation_screen.dart';
 import 'package:spark_app/application/login/loginviaguest/landing_bloc.dart';
-import 'package:spark_app/application/login/loginviaguest/landing_state.dart';
 import 'package:spark_app/core/api/api_service.dart';
-import 'package:spark_app/core/repository/dashboardrepository/accountrepository/account_repository.dart';
-import 'package:spark_app/core/repository/dashboardrepository/activityrepository/activity_repository.dart';
-import 'package:spark_app/core/repository/dashboardrepository/homerepository/home_repository.dart';
-import 'package:spark_app/core/repository/dashboardrepository/messagerepository/message_repository.dart';
-import 'package:spark_app/core/repository/dashboardrepository/paymentrepository/payment_repository.dart';
 import 'package:spark_app/core/repository/loginrepository/login_repository.dart';
 import 'package:spark_app/core/routes/routes.dart';
 
